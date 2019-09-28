7ea1e029828fc711d0808fae15d0ea1375591578
Coditum <coditum@ST-C02SD0QNFVH4.local>
2019-09-28 15:12:40 -0400

    1

---
+++ js/init-firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCe140ZKf_5z6j43KuSoPbFhaqKu5_keAY",
+    authDomain: "talk-of-the-tee-site.firebaseapp.com",
+    databaseURL: "https://talk-of-the-tee-site.firebaseio.com",
+    projectId: "talk-of-the-tee-site",
+    storageBucket: "talk-of-the-tee-site.appspot.com",
+    messagingSenderId: "923599241680",
+    appId: "1:923599241680:web:70328fb7db99e86dcc39d5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
